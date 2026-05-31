8a61988c4640fe9ad2529e4a678f9e8408766fbc
RUTH TESSU <tessuruth540@gmail.com>
2026-05-31 22:28:07 +0100

    implement initials printing functionality and add assignment details

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: print the initials of a name
+# Assignment Number: 1
+#
+# Name: RUTH TESSU
+# STUDENT ID:  2425404251
+# Email: 2425404251@live.gctu.edu
+# Grader: RUTH
+#
+# On my honor, RUTH TESSU, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
-
-
+    # Print the initials of my name
+    print()
+    print("...RT")
+    print()
+    print("...RRRRRRR...........TTTTTTTTTTTT.....")
+    print("...RRRRRRR...........TTTTTTTTTTTT.....")
+    print("...R......RRR.............TT..........")
+    print("...R......RRR.............TT..........")
+    print("...RRRRRRR................TT..........")
+    print("...RRRRRRR................TT..........")
+    print("...R......RRR.............TT..........")
+    print("...R......RRR.............TT..........")
+    print("...R......RRR...**........TT........**")
+    print("...R......RRR...**........TT........**")
+    print()
+    
+    
+    
 main()
